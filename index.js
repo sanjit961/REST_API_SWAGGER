2310552c83415eb09951f1f26cf2a2972943b489
Sanjit Lungeli <sanjitlungeli241@gmail.com>
2023-02-05 17:43:05 +0530

    sdfd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDoc = require("./swagger.json");
 require("dotenv").config();
-const db = require('./config/config').get(process.env.NODE_ENV)
+const db = require("./config/config").get(process.env.NODE_ENV);
 const app = express();
 mongoose.connect(db.MONGO_URL);
 const database = mongoose.connection;
@@ -19,8 +19,11 @@ app.use(express.json());
 const routes = require("./route/route");
 app.use("/", routes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.get("/", (req, res) => {
+  res.json({ message: "Hello world" });
+});
 const SWAGGER_DOC = `http://localhost:3001}/api-docs`;
-app.listen(db.PORT || 3001, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log(`API DOCUMENTATIONS: ${SWAGGER_DOC}`);
   console.log("The server started on port: ", 3001);
 });
